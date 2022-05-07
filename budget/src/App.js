6376460bc25c7668c04ecc1ca79680e102dfb0b1
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-07 23:36:41 +0900

    useEffect問題

--- budget/src/App.js
+++ budget/src/App.js
@@ -11,8 +11,11 @@ import EditEntryForm from './components/EditEntryForm';
 import ModalEdit from './components/ModalEdit';
 
 function App() {
-  const [entry, setEntry] = useState({});
+  // サーバから最新の情報をGETEするステートと、新規/更新/削除した場合のステートを分けることでいったん落ち着く。
+  const [latestEntries, setLatestEntries] = useState([]);
   const [entries, setEntries] = useState([]);
+
+  const [entryToUpdate, setEntryToUpdate] = useState({});
   const [income, setIncome] = useState(0);
   const [expense, setExpense] = useState(0);
   const [balance, setBalance] = useState(0);
@@ -21,13 +24,13 @@ function App() {
   useEffect(() => {
     const fetchBudgets = async () => {
       const res = await budgets.get('/budgets');
-      setEntries(res.data);
+      setLatestEntries(res.data);
     };
     fetchBudgets();
-  }, [entry]);
+  }, [entries]);
 
   useEffect(() => {
-    const { calcIncome, calcExpense } = entries.reduce(
+    const { calcIncome, calcExpense } = latestEntries.reduce(
       (acc, entry) => {
         if (entry.isExpense) {
           return { ...acc, calcExpense: acc.calcExpense + Number(entry.value) };
@@ -43,25 +46,25 @@ function App() {
     setIncome(calcIncome);
     setExpense(calcExpense);
     setBalance(calcIncome - calcExpense);
-  }, [entries]);
+  }, [latestEntries]);
 
   const addEntry = async (entry) => {
     const entryToUpdate = { ...entry, id: uuidv4() };
     await budgets.post('/budgets', entryToUpdate);
 
-    setEntry(entryToUpdate);
+    setEntries({ ...entries, ...entryToUpdate });
   };
 
   const editEntry = (id) => {
-    const entryToUpdate = entries.find((entry) => entry.id === id);
-    setEntry(entryToUpdate);
+    const entryToUpdate = latestEntries.find((entry) => entry.id === id);
+    setEntryToUpdate(entryToUpdate);
     setIsOpen(true);
   };
 
   const editEntryAndCloseModal = async (entryToUpdate) => {
     await budgets.patch(`/budgets/${entryToUpdate.id}`, entryToUpdate);
 
-    setEntry(entryToUpdate);
+    setEntries({ ...entries, ...entryToUpdate });
     setIsOpen(false);
   };
 
@@ -71,7 +74,9 @@ function App() {
 
   const deleteEntry = async (id) => {
     await budgets.delete(`/budgets/${id}`);
-    setEntry({});
+
+    const entryToUpdate = latestEntries.find((entry) => entry.id === id);
+    setEntries({ ...entries, ...entryToUpdate });
   };
 
   return (
@@ -85,13 +90,16 @@ function App() {
 
       <MainHeader type="h3" title="History" />
       <EntryLines
-        entries={entries}
+        entries={latestEntries}
         onEditEntry={editEntry}
         onDeleteEntry={deleteEntry}
       />
 
       <ModalEdit isOpen={isOpen} onClose={onClose}>
-        <EditEntryForm entry={entry} onEditEntry={editEntryAndCloseModal} />
+        <EditEntryForm
+          entry={entryToUpdate}
+          onEditEntry={editEntryAndCloseModal}
+        />
       </ModalEdit>
     </Container>
   );
