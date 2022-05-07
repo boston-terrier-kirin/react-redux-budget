69742216e8848eaad80de2a0931952504d56bb49
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-07 21:58:04 +0900

    budget/Section4-29

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
@@ -9,16 +9,38 @@ import ModalEdit from './components/ModalEdit';
 import NewEntryForm from './components/NewEntryForm';
 
 const initialState = [
-  { id: 1, description: 'Work Income', value: '$2000.00', isExpense: false },
-  { id: 2, description: 'Water Bill', value: '$10.00', isExpense: true },
-  { id: 3, description: 'House Rent', value: '$200.00', isExpense: true },
-  { id: 4, description: 'Power Bill', value: '$100.00', isExpense: true },
+  { id: 1, description: 'Work Income', value: 2000.0, isExpense: false },
+  { id: 2, description: 'Water Bill', value: 10.0, isExpense: true },
+  { id: 3, description: 'House Rent', value: 200.0, isExpense: true },
+  { id: 4, description: 'Power Bill', value: 100.0, isExpense: true },
 ];
 
 function App() {
   const [entry, setEntry] = useState({});
   const [entries, setEntries] = useState(initialState);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [balance, setBalance] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const { calcIncome, calcExpense } = entries.reduce(
+      (acc, entry) => {
+        if (entry.isExpense) {
+          return { ...acc, calcExpense: acc.calcExpense + Number(entry.value) };
+        }
+        return { ...acc, calcIncome: acc.calcIncome + Number(entry.value) };
+      },
+      {
+        calcIncome: 0,
+        calcExpense: 0,
+      }
+    );
+
+    setIncome(calcIncome);
+    setExpense(calcExpense);
+    setBalance(calcIncome - calcExpense);
+  }, [entries]);
 
   const deleteEntry = (id) => {
     const updatedEntries = entries.filter((entry) => entry.id !== id);
@@ -51,8 +73,8 @@ function App() {
   return (
     <Container>
       <MainHeader title="Budget" />
-      <DisplayBalance title="Your Balance" value="2,550.53" size="small" />
-      <DisplayBalances />
+      <DisplayBalance title="Your Balance" value={balance} size="small" />
+      <DisplayBalances income={income} expense={expense} />
 
       <MainHeader type="h3" title="Add New Transaction" />
       <NewEntryForm onAddEntry={addEntry} />
--- budget/src/components/DisplayBalances.js
+++ budget/src/components/DisplayBalances.js
@@ -1,16 +1,16 @@
 import { Grid, Segment } from 'semantic-ui-react';
 import DisplayBalance from './DisplayBalance';
 
-const DisplayBalances = () => {
+const DisplayBalances = ({ income, expense }) => {
   return (
     <Segment textAlign="center">
       <Grid columns="2" divided>
         <Grid.Row>
           <Grid.Column>
-            <DisplayBalance title="Income" value="624.00" color="green" />
+            <DisplayBalance title="Income" value={income} color="green" />
           </Grid.Column>
           <Grid.Column>
-            <DisplayBalance title="Expense" value="134.00" color="red" />
+            <DisplayBalance title="Expense" value={expense} color="red" />
           </Grid.Column>
         </Grid.Row>
       </Grid>
